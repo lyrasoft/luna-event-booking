613011df7ac5385fff0ef4ce64d8202dc82dbcc6
Simon Asika <asika32764@gmail.com>
2024-07-17 16:45:01 +0800

    is publish up

--- src/Service/EventAttendingService.php
+++ src/Service/EventAttendingService.php
@@ -99,7 +99,7 @@ class EventAttendingService
 
         if (
             $plan->getStageId() !== $stage->getId()
-            || ($plan->isPublishUp() && $plan->isPublishUp()?->isFuture())
+            || !$plan->isPublishUp()
         ) {
             throw new ValidateFailException('Plan is invalid');
         }
