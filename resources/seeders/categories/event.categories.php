dd1f01c42873882fb7021005d72c2893b71f39e5
Simon Asika <asika32764@gmail.com>
2025-11-10 14:18:01 +0800

    categories

---
+++ resources/seeders/categories/event.categories.php
@@ -0,0 +1,14 @@
+<?php
+
+declare(strict_types=1);
+
+return [
+    'event' => [
+        'max_level' => 1,
+        'number' => 10,
+    ],
+    'venue' => [
+        'max_level' => 1,
+        'number' => 5,
+    ],
+];
